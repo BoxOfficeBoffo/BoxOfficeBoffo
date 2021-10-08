469f1eb2e3e24e7edefd30bb8768890b71504d27
Jasmine <jacamoe@live.ca>
2021-10-08 17:03:52 -0400

    Pseudocode added to project

--- src/App.js
+++ src/App.js
@@ -9,3 +9,19 @@ function App() {
 }
 
 export default App;
+
+// PSEUDOCODE
+// 1. Add user interface, utilizing form elements
+// 2. Based on the summer year(between May 1-September 4), an API call be made based on the user's input
+  // - User will be given option to search
+  // - Filter movies by year and store the top 10(by revenue) in to a new Array
+  // - User prompted with alert if selection is empty to make a choice
+// 3. User can select 10 movies to be ranked later
+  // - Button to confirm selection
+// 4. Using a Router, users will be routed to a page with their selected movies
+  // - User can then rank their selection from 1-10
+  // - Final ranked selection will be pushed to firebase, and unchangeable
+  // - User will be given an option to see results or create another list or delete list
+    // -Before deleting a prediction list, user will be prompted with a custom confirmation box "Are you sure?"
+// 5. User is then Routed to page with user selection and officual order of highest grossing films to compare
+
