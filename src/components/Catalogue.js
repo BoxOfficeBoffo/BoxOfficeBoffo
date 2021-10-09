b00d22ec7dcbac02f4d326a395ac6d7984766998
Jasmine <jacamoe@live.ca>
2021-10-09 12:21:08 -0400

    Made API call using axios

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,16 +1,38 @@
 
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import DisplayCatalogue from './DisplayCatalogue';
 
 const Catalogue = () => {
 
-  const [userInput, setUserInput] = useState()
+  const [userInput, setUserInput] = useState();
+
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+
+    axios({
+      url: 'https://api.themoviedb.org/3/discover/movie',
+      params: {
+        api_key: 'da4fdac82c009adaed8ec1f39b233b93',
+        language: 'en-US',
+        sort_by: 'original_title.asc',
+        include_adult: 'false',
+        include_video: 'false',
+        page: 1,
+        primary_release_year: 2010,
+      }
+    }).then((res) => {
+      console.log(res.data.results)
+      setMovies(res.data.results);
+
+    })
+  }, []);
 
   const handleChange = (e) => {
     setUserInput(e.target.value);
-    console.log(e.target.value);
   }
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
   }
@@ -20,12 +42,12 @@ const Catalogue = () => {
       <div className="catalogueForm">
         <form onSubmit={handleSubmit}>
           <label htmlFor="yearInput">Choose a year:</label>
-          <input 
-          type="number"
-          id="yearInput"
-          onChange={handleChange}
-          value={userInput}
-          placeholder="1999" />
+          <input
+            type="text"
+            id="yearInput"
+            onChange={handleChange}
+            value={userInput}
+            placeholder="1999" />
           <button type="submit">Enter</button>
         </form>
       </div>
---
+++ src/components/DisplayCatalogue.js
@@ -0,0 +1,8 @@
+const DisplayCatalogue = () => {
+    return (
+        <h2>This is a movie</h2>
+    )
+}
+
+export default DisplayCatalogue
+
